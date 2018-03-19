c7638367142813e2ad248271ea62fe357e685a1a
Danielle H <daheber@gmail.com>
2018-03-18 20:05:40 -0700

    update landing page

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { API } from "aws-amplify";
 
@@ -65,8 +66,16 @@ export default class Home extends Component {
   renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
+        <h1>Take Note!</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
